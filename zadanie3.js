f0698af47ec661926331a51e596da9edca93cff1
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-26 22:10:35 +0100

    do programu dodaje oceny po dodaniu i wypisanie ich jeszcze raz

--- zadanie3.js
+++ zadanie3.js
@@ -2,15 +2,22 @@ function student(imie,wiek,oceny){
     console.log(`Imie ucznia: ${imie}`);
     console.log(`wiek ucznie: ${wiek}`);
     console.log(`Oceny ucznia: ${oceny}`);
-
+    
     const sum = oceny.reduce((acc, num) => acc + num, 0);
     const dlugoscArray = oceny.length
     const srednia = sum / dlugoscArray
 
     console.log(`srednia ocen ucznia ${imie} to: ${srednia}`)
+
+    
 }
 
 let a = "Karol";
 let b = 18;
 const ocena = [1,3,2,2,5];
 student(a,b,ocena)
+
+
+const dodajOcena = [2,10,20,4,5]; //uzytkownik inputem dodwalby tu oceny
+const ocenyPoDodaniu = [...ocena,...dodajOcena];
+student(a,b,ocenyPoDodaniu)

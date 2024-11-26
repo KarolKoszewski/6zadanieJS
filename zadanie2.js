4fcb41cbb28f074908eb290b1bdf1a24432a5a0a
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-26 21:40:13 +0100

    Dodanie danych, wyboru działania oraz funkcji dla kazdego działania

---
+++ zadanie2.js
@@ -0,0 +1,20 @@
+pierwszaLiczba = 100; // normalnie uzytkowanik podwalby sam te wartosci
+drugaLiczba = 2;
+podana = "%";
+
+function add(a,b){
+    return a+b;
+}
+function substruct(a,b){
+    return a-b
+}
+function multipy(a,b){
+    return a*b
+}
+function divide(a,b){
+    if(b===0){
+        return "nie dziel przez zero"
+    }else{
+        return a / b
+    }
+}

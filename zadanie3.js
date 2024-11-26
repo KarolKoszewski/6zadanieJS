88f1a61959789396373da59e1be237a7cb5409c0
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-26 22:03:50 +0100

    dodaje w funkcji liczenie sumy i liczenie sredniej

--- zadanie3.js
+++ zadanie3.js
@@ -1,7 +1,13 @@
 function student(imie,wiek,oceny){
     console.log(`Imie ucznia: ${imie}`);
     console.log(`wiek ucznie: ${wiek}`);
-    console.log(`Oceny ucznia: ${oceny}`)
+    console.log(`Oceny ucznia: ${oceny}`);
+
+    const sum = oceny.reduce((acc, num) => acc + num, 0);
+    const dlugoscArray = oceny.length
+    const srednia = sum / dlugoscArray
+
+    console.log(`srednia ocen ucznia ${imie} to: ${srednia}`)
 }
 
 let a = "Karol";

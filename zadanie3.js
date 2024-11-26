7027271c333b3ddda251713cc67113573c1acabd
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-26 21:55:34 +0100

    w zadaniu 3 robie funkcje student która przyjmuje dane ucznia i je wypisuje

---
+++ zadanie3.js
@@ -0,0 +1,10 @@
+function student(imie,wiek,oceny){
+    console.log(`Imie ucznia: ${imie}`);
+    console.log(`wiek ucznie: ${wiek}`);
+    console.log(`Oceny ucznia: ${oceny}`)
+}
+
+let a = "Karol";
+let b = 18;
+const ocena = [1,3,2,2,5];
+student(a,b,ocena)

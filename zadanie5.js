c6d10e2c079eb0e872fbb9076bda0718f3006a56
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-27 14:52:27 +0100

    dodaje showCar i wywoluje narazie czesc rzeczy

--- zadanie5.js
+++ zadanie5.js
@@ -8,5 +8,16 @@ class Cart{
     }
     removeProduct(productDoKasacji){
         this.produkt = this.produkt.filter(produkt => produkt !== productDoKasacji)
+        console.log(`Kasacja poduktu: ${productDoKasacji}`)
+    }
+
+    showCart(){
+        console.log(`Lista produktów w koszyku ${this.produkt}`);
     }
 }
+
+const Koszyczek = new Cart();
+Koszyczek.addProduct("Jaja");
+Koszyczek.addProduct("Majonez");
+Koszyczek.addProduct("Ser");
+Koszyczek.showCart();

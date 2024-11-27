76c6f7484afb10130e87a1e4b9ffbf4aaa052279
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-27 14:04:37 +0100

    zadanie1 dodanie zmienej i funkcji

---
+++ zadanie1.js
@@ -0,0 +1,13 @@
+let text = "Idzie kominiarz po drabinie fiku miku już w kominie!";
+
+const textEdit = (a,b) => a
+
+
+const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
+    if(uppercase == true){
+        text.uppercase();
+    }
+    
+} 
+
+processText('Hello world', {uppercase}, {reverse})

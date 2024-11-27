9072e75c997148f162ee080eff53142681b47159
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-27 14:15:51 +0100

    robie poprawki wczesniejszego kodu

--- zadanie1.js
+++ zadanie1.js
@@ -1,13 +1,10 @@
-let text = "Idzie kominiarz po drabinie fiku miku już w kominie!";
-
-const textEdit = (a,b) => a
-
 
 const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
     if(uppercase == true){
-        text.uppercase();
+        text = text.toUpperCase();
     }
     
+    console.log(`twój tekst po zmianach: ${text}`)
 } 
 
-processText('Hello world', {uppercase}, {reverse})
+processText('Hello world!', { uppercase: true,reverse: true ,removeSpecialChars: true });

348b0ea5c4145caad09e46c778359ed6a89b9b8a
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-28 10:56:52 +0100

    zastosowałem zmiany w zadaniu2

--- zadanie2.js
+++ zadanie2.js
@@ -1,44 +1,47 @@
 
 class kalkulator{
-    constructor(a,b,dzialanie){
+    constructor(a,b){
         this.a = a;
         this.b = b;
     }
     
-    add(){
-        console.log(`Wynik dodawania to: ${this.a + this.b}`)
+    add(a,b){
+        console.log(`Wynik dodawania to: ${a + b}`)
     }
-    substruct(){
-        console.log(`Wynik odjemowania to: ${this.a - this.b}`)
+    substruct(a,b){
+        console.log(`Wynik odjemowania to: ${a - b}`)
     }
-    multipy(){
-        console.log(`Wynik mnożenia to: ${this.a * this.b}`)
+    multipy(a,b){
+        console.log(`Wynik mnożenia to: ${a * b}`)
     }
-    divide(){
-        console.log(`Wynik dzielenia to: ${this.a / this.b}`)
+    divide(a,b){
+        console.log(`Wynik dzielenia to: ${a / b}`)
     }
 }
-const dodawanie = new kalkulator();
-kalkulator.add(1,2)
-// switch (podana)
-// {
-//     case "+":
-//         console.log("Wynik dodania: "+add(pierwszaLiczba, drugaLiczba));
-//         break;
+const dzialanie = new kalkulator();
+let pierwszaLiczba = 200;// Użytkownik inputem podawałby liczbe
+let drugaLiczba = 10;// Użytkownik inputem podawałby liczbe
+let podana = "/"; // Użytkownik inputem podawałby jakie działa chce wykonać
+switch (podana)
+{
+    case "+":
 
-//     case "-":
-//         console.log("Wynik odejmowania: "+substruct(pierwszaLiczba, drugaLiczba));
-//         break;
+        dzialanie.add(pierwszaLiczba, drugaLiczba);
+        break;
 
-//     case "*":
-//         console.log("Wynik mnożenia: "+multipy(pierwszaLiczba, drugaLiczba));
-//         break;
+    case "-":
+        dzialanie.substruct(pierwszaLiczba, drugaLiczba);
+        break;
 
-//     case "%":
-//         console.log("Wynik dzielenia: "+divide(pierwszaLiczba, drugaLiczba));
-//         break;
+    case "*":
+        dzialanie.multipy(pierwszaLiczba, drugaLiczba);
+        break;
 
-//     default:
-//         console.log("nasz kalkulator nie obsługuje takie działania!!");
-//         break;
-//     }
+    case "/":
+        dzialanie.divide(pierwszaLiczba, drugaLiczba)
+        break;
+
+    default:
+        console.log(`nasz kalkulator nie obsługuje takie działania!!`);
+        break;
+    }

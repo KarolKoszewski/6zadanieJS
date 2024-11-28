75fdd93f65234fa7cf2f8ec948a89b54eea3471b
Karol Koszewski <karol.koszewski1@gmail.com>
2024-11-28 11:22:37 +0100

    Wprowadzam troche zmian ale zadanie3 jeszcze nie skonczone

--- zadanie3.js
+++ zadanie3.js
@@ -1,36 +1,21 @@
-// function student(imie,wiek,oceny){
-//     console.log(`Imie ucznia: ${imie}`);
-//     console.log(`wiek ucznie: ${wiek}`);
-//     console.log(`Oceny ucznia: ${oceny}`);
-    
-//     const sum = oceny.reduce((acc, num) => acc + num, 0);
-//     const dlugoscArray = oceny.length
-//     const srednia = sum / dlugoscArray
-
-//     console.log(`srednia ocen ucznia ${imie} to: ${srednia}`)
-
-    
-// }
-
-// let a = "Karol";
-// let b = 18;
-// const ocena = [1,3,2,2,5];
-// student(a,b,ocena)
-
-
-// const dodajOcena = [2,10,20,4,5]; //uzytkownik inputem dodwalby tu oceny
-// const ocenyPoDodaniu = [...ocena,...dodajOcena];
-// student(a,b,ocenyPoDodaniu)
-
 class student{
     constructor(imie, wiek, oceny, dodaj,ocenyPoDodaniu){
-        this.oceny = [1,2,3,4,4,4];
-        this.dodaj = [];
+        this.oceny = [1,2,3,4,5];
+        this.dodaj = dodaj;
         this.ocenyPoDodaniu = [];
 
     }
     addGrade(dodaj){
-        dodaj = [a,b];
-        ocenyPoDodaniu = [...this.oceny, ...dodaj]
+        
+        this.ocenyPoDodaniu = [...this.oceny, ...dodaj]
+        console.log(`Oceny po dodaniu: ${this.ocenyPoDodaniu}`);
+    }
+    wypiszDane(imie,wiek){
+        console.log(`Imie: ${imie}`);
+        console.log(`Wiek: ${wiek}`);
+        console.log(`Oceny: ${this.oceny}`);
     }
 }
+ const uczen = new student();
+ uczen.wypiszDane("Karol", 17)
+ uczen.addGrade(5)
